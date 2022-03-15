f327efa643735476bed14d1450a315c053a4af3b
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-03-15 21:57:32 +0000

    re style input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -10,8 +10,23 @@ function Input() {
                     type="text"
                     placeholder="Type a message..."
                 />
+
                 <button className={style.send}>
-                    <img src="/send.png" alt="send" />
+                    <div className={style.svgWrapper}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                        >
+                            <path fill="none" d="M0 0h24v24H0z"></path>
+                            <path
+                                fill="currentColor"
+                                d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <span>Send</span>
                 </button>
             </div>
         </div>
