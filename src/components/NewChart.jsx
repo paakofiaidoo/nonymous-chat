431a15b0be2ff2ac121c21b97c05adff88270791
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-03-15 20:02:23 +0000

    setup header

--- src/components/NewChart.jsx
+++ src/components/NewChart.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Menu from "./Menu";
 import Input from "./Input";
 import Messages from "./Messages";
+import style from "../styles/layout.module.scss";
 
 function NewChart() {
     return (
-        <layout>
-            <header></header>
-            <Menu></Menu>
+        <layout className={style.layout}>
+            <header className={style.head}>
+                <h2>Nonymous</h2>
+                <Menu />
+            </header>
+
             <main>
                 <Messages></Messages>
                 <Input></Input>
