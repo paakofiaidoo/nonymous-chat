26cf18c327faab178fd8979ef99cd26dcb6d9864
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-03-15 15:31:32 +0000

    fixed messagaes area

---
+++ src/components/Messages/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MessageCard from "../MessageCard";
+import styles from "./styles.module.scss";
+
+function Messages() {
+    let messages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return (
+        <div className={styles.messages}>
+
+            {messages.map((message) => {
+                return <MessageCard />;
+            })}
+            <MessageCard></MessageCard>
+        </div>
+    );
+}
+
+export default Messages;
